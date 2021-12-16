ab44e284af120725bc3f654f58d34765c6d8acaf
Mark Roduner <mark@energyservices.org>
2021-12-16 10:09:02 -0800

    Initial Commit

---
+++ ActiveDirectory.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.DirectoryServices;
+
+namespace Brat.Drivers {
+    public class ActiveDirectory : Driver {
+        private DriverAPI api;
+        public readonly DirectoryEntry Connection;
+        
+        public ActiveDirectory(string profile_name = "") : base(profile_name) {
+            //Settings = PersistantSettings.Get<ActiveDirectorySettings>();
+            DirectoryEntry ad_Info  = new DirectoryEntry("LDAP://RootDSE");
+            Connection = new DirectoryEntry($"LDAP://{ad_Info.Properties["defaultNamingContext"][0]}");
+            api = new ActiveDirectoryAPI(this);
+        }
+        public override DriverAPI GetAPI() {
+            return api;
+        }
+    }
+    public partial class ActiveDirectorySettings : PersistantSettings {
+        public ActiveDirectorySettings() : base("activedirectory") { }
+    }
+    public partial class ActiveDirectoryAPI : DriverAPI {
+        private ActiveDirectory Driver;
+        public ActiveDirectoryAPI(ActiveDirectory instance) { Driver = instance;  }
+    }
+}
